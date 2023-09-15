39e7a4a1381f224ee043f84bdbc3c329c5058cfb
Andrew Kaiser <Andrewkaiser2006@gmail.com>
2023-09-15 09:26:31 -0500

    setup

--- CafeCollectionKaiser/ViewController.swift
+++ CafeCollectionKaiser/ViewController.swift
@@ -8,12 +8,30 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    //Images
+    @IBOutlet weak var cartOutlet: UIImageView!
+    @IBOutlet weak var cartCounterOutlet: UILabel!
+    @IBOutlet weak var chickenOutlet: UIImageView!
+    @IBOutlet weak var eggOutlet: UIImageView!
+    @IBOutlet weak var hotdogOutlet: UIImageView!
+    @IBOutlet weak var chipOutlet: UIImageView!
+    @IBOutlet weak var milkOutlet: UIImageView!
+    //name and quantity fields
+    @IBOutlet weak var nameFieldOutlet: UITextField!
+    @IBOutlet weak var priceFieldOutlet: UITextField!
+    //
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        cartOutlet.image = UIImage(named: "cart")
+        chickenOutlet.image = UIImage(named: "chicken")
+        eggOutlet.image = UIImage(named: "eggs")
+        hotdogOutlet.image = UIImage(named: "hotdog")
+        chipOutlet.image = UIImage(named: "chips")
+        milkOutlet.image = UIImage(named: "milk")
     }
-
+    var menu = ["Chicken","Egg","Hotdog","Chip","Milk"]
+    var menuPrice = [10,5,3,1,6]
 
 }
 
